94554bb61f97de75410d743eafba22454d0be53c
Tim Quimbo <tquimbo1@gmail.com>
2021-09-22 00:32:11 -0400

    Added post sessions method.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,10 +4,19 @@ class SessionsController < ApplicationController
         erb :"sessions/new.html"
       end
 
+      post "/sessions" do
+        user = User.find_by_username(params[:username])
+        if user && user.authenticate(params[:password])
+          session[:user_id] = user.id
+          redirect "/"
+        else
+          redirect "/login"
+        end
+      end
+
       get "/logout" do
         session.clear
         redirect "/"
       end
-
 
     end
