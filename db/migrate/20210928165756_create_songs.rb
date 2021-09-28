e3e3af5c276c153b3ddfc7b508815e16e8abd2df
Tim Quimbo <tquimbo1@gmail.com>
2021-09-28 14:55:28 -0400

    Add songs and user id database

--- db/migrate/20210928165756_create_songs.rb
+++ db/migrate/20210928165756_create_songs.rb
@@ -1,7 +1,10 @@
-class CreateSongs < ActiveRecord::Migration
+class CreateSongs < ActiveRecord::Migration[5.2]
   def change
     create_table :songs do |t|
-
+      t.string :name
+      t.string :artist
+      t.string :album
+      t.integer :rating
       t.timestamps null: false
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_09_21_015857) do
+ActiveRecord::Schema.define(version: 2021_09_28_174615) do
+
+  create_table "songs", force: :cascade do |t|
+    t.string "name"
+    t.string "artist"
+    t.string "album"
+    t.integer "rating"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "username"
