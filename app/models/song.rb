e92e862cacb0aeab6fbb3a295b31645458c360f8
Tim Quimbo <tquimbo1@gmail.com>
2021-09-29 15:59:10 -0400

    Validations to user model class

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,3 +1,6 @@
 class Song < ActiveRecord::Base
     belongs_to :user
+    validates :name, :artist, :album, presence: :true
+    validates :rating, numericality: { greater_than_or_equal_to: 0, less_than_or_equal_to: 10 }
+    validates :user_id, presence: :true
 end
