c1721900ef63d074c2586c816f2722f691b44b38
Tim Quimbo <tquimbo1@gmail.com>
2021-09-29 15:17:31 -0400

    Add songs new submission form.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,25 +1,22 @@
 class SongsController < ApplicationController
 
-    get "/songs" do
-        @songs = Songs.all
-        erb :"songs/index.html"
-    end
+    # get "/songs" do
+    #     @songs = Songs.all
+    #     erb :"songs/index.html"
+    # end
 
     get "/songs" do
         if params[:search]
-        # if we're searching, only render some sharks
           @songs = Song.where('name LIKE ?', "%#{params[:search]}%")
         else
-        # otherwise render all sharks
           @songs = Song.all
         end
         erb :"songs/index.html"
       end
 
       get "/songs/new" do
-        redirect_if_not_logged_in
-        @songs = Song.new
-        erb :"sharks/new.html"
+        @song = Song.new
+        erb :"songs/new.html"
       end
       
    
