596a95000f9c2f72753b4dd215e779b9a042a77c
Tim Quimbo <tquimbo1@gmail.com>
2021-11-04 12:42:38 -0400

    Review 1

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,56 +1,56 @@
 class SongsController < ApplicationController
 
-    get "/songs/new" do
-       redirect_if_not_logged_in
-      @song = Song.new
-      erb :"songs/new.html"
-    end
-
-    post "/songs" do
-      song = current_user.songs.create(params[:song])
-      redirect "/songs/#{song.id}"
-    end   
-
-    get "/songs" do
-        if params[:search]
-          @songs = Song.where('name LIKE ?', "%#{params[:search]}%")
-        else
-          @songs = Song.all
-        end
-        erb :"songs/index.html"
-    end
-    
-    get "/songs/:id" do
-      @song = Song.find(params[:id])
-      erb :"songs/show.html"
-    end
-
-    get "/songs/:id/edit" do
-          redirect_if_not_logged_in
-          @song = Song.find(params[:id])
-          erb :"songs/edit.html"
-    end  
-
-    patch "/songs/:id/edit" do
-        song = Song.find(params[:id])
-        if song.user == current_user
-          if song.update(params[:song])
-            redirect "/songs/#{song.id}"
-          else
-            flash[:errors] = song.errors.full_messages
-            redirect "/songs/#{song.id}/edit"
-          end
-          redirect "/songs"
-        end
-     end
-  
-  delete "/songs/:id" do
-    song = Song.find(params[:id])
-    if song.user == current_user
-    song.destroy
-    redirect "/songs"
+  get "/songs/new" do
+     redirect_if_not_logged_in
+    @song = Song.new
+    erb :"songs/new.html"
   end
-    
+
+  post "/songs" do
+    song = current_user.songs.create(params[:song])
+    redirect "/songs/#{song.id}"
+  end   
+
+  get "/songs" do
+      if params[:search]
+        @songs = Song.where('name LIKE ?', "%#{params[:search]}%")
+      else
+        @songs = Song.all
+      end
+      erb :"songs/index.html"
+  end
+  
+  get "/songs/:id" do
+    @song = Song.find(params[:id])
+    erb :"songs/show.html"
+  end
+
+  get "/songs/:id/edit" do
+        redirect_if_not_logged_in
+        @song = Song.find(params[:id])
+        erb :"songs/edit.html"
+  end  
+
+  patch "/songs/:id" do
+      song = Song.find(params[:id])
+      if song.user == current_user
+        if song.update(params[:song])
+          redirect "/songs/#{song.id}"
+        else
+          flash[:errors] = song.errors.full_messages
+          redirect "/songs/#{song.id}/edit"
+        end
+        redirect "/songs"
+      end
+   end
+
+delete "/songs/:id" do
+  song = Song.find(params[:id])
+  if song.user == current_user
+  song.destroy
+  redirect "/songs"
+end
+  
 
 end
 
